eb237ae7c91b52b422e68cb7a7db1a8830f546c0
Labranche <slabranche@cmontmorency.qc.ca>
2023-01-30 06:52:14 -0500

    objectifs vide

---
+++ exempleObjectifsVide/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ListeObjectifs from './components/ListeObjectifs';
+import './css/App.css';
+
+function App(){
+  const objectifsCours = [
+    {id: 'cg1', texte: 'Apprendre React', importance:"4"},
+    {id: 'cg2', texte: 'Apprendre Node.Js', importance:"4"},
+    {id: 'cg3', texte: 'Faire un site Web', importance:"1"},
+  ];
+
+  return (
+    <div className="course-goals">
+      <h2>Objectifs du cours</h2>
+      <ListeObjectifs objectifs={objectifsCours} />
+    </div>
+  );
+};
+
+export default App;
